b3931585330f8f14d4cdfad67fa352ca2e1f1cf1
AngelIsaiSzr <angelmoreno152008@gmail.com>
2024-05-25 00:01:13 -0600

    First Commit

--- js/index.js
+++ js/index.js
@@ -126,7 +126,7 @@ $(function () {
     gsap.to('#navigation-content', 0, { y: '-100%', delay: .7 });
     gsap.to('#header', 0, { display: "none" });
     gsap.to('#gallery', 0, { display: "none" });
-    gsap.to('#portfolio', 0, { display: "none" });
+    gsap.to('#progress', 0, { display: "none" });
     gsap.to('#breaker', 0, { display: "block" });
     gsap.to('#breaker-two', 0, { display: "block", delay: .1 });
     gsap.to('#contact', 0, { display: "none" });
@@ -142,7 +142,7 @@ $(function () {
     gsap.to('#header', 0, { display: "none" });
     gsap.to('#about', 0, { display: "none" });
     gsap.to('#gallery', 0, { display: "none" });
-    gsap.to('#portfolio', 0, { display: "none" });
+    gsap.to('#progress', 0, { display: "none" });
     gsap.to('#breaker', 0, { display: "block" });
     gsap.to('#breaker-two', 0, { display: "block", delay: .1 });
     gsap.to('#breaker', 0, { display: "none", delay: 2 });
@@ -150,7 +150,7 @@ $(function () {
     gsap.to('#contact', 0, { display: "block", delay: .7 });
     gsap.to('#navigation-content', 0, { display: 'flex', delay: 2 });
   });
-  $('#portfolio-link').on('click', function () {
+  $('#progress-link').on('click', function () {
     gsap.to('#navigation-content', 0, { display: "none", delay: .7 });
     gsap.to('#navigation-content', 0, { y: '-100%', delay: .7 });
     gsap.to('#header', 0, { display: "none" });
@@ -161,7 +161,7 @@ $(function () {
     gsap.to('#breaker-two', 0, { display: "block", delay: .1 });
     gsap.to('#breaker', 0, { display: "none", delay: 2 });
     gsap.to('#breaker-two', 0, { display: "none", delay: 2 });
-    gsap.to('#portfolio', 0, { display: "block", delay: .7 });
+    gsap.to('#progress', 0, { display: "block", delay: .7 });
     gsap.to('#navigation-content', 0, { display: 'flex', delay: 2 });
   });
 
@@ -170,7 +170,7 @@ $(function () {
     gsap.to('#navigation-content', 0, { y: '-100%', delay: .7 });
     gsap.to('#header', 0, { display: "none" });
     gsap.to('#about', 0, { display: "none" });
-    gsap.to('#portfolio', 0, { display: "none" });
+    gsap.to('#progress', 0, { display: "none" });
     gsap.to('#contact', 0, { display: "none" });
     gsap.to('#breaker', 0, { display: "block" });
     gsap.to('#breaker-two', 0, { display: "block", delay: .1 });
@@ -185,7 +185,7 @@ $(function () {
     gsap.to('#navigation-content', 0, { y: '-100%', delay: .7 });
     gsap.to('#header', 0, { display: "none" });
     gsap.to('#about', 0, { display: "none" });
-    gsap.to('#portfolio', 0, { display: "none" });
+    gsap.to('#progress', 0, { display: "none" });
     gsap.to('#contact', 0, { display: "none" });
     gsap.to('#gallery', 0, { display: "none" });
     gsap.to('#breaker', 0, { display: "block" });
