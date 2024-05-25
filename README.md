0e1f86a4b2d1b8be85e5a32ffc781e02bbb8bee3
AngelIsaiSzr <angelmoreno152008@gmail.com>
2024-05-24 22:24:40 -0600

    First Commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# 🗺️Programa Aula Escuela y Comunidad (PAEC) en el Parque Xanath🌳
+
+## Resumen
+El Programa Aula Escuela y Comunidad (PAEC) es una iniciativa llevada a cabo en el Parque Xanath por alumnos del Colegio de Estudios Científicos y Tecnológicos del Estado de Veracruz, 02 Papantla CECyTEV. Este programa tiene como objetivo promover la educación ambiental y el cuidado del entorno natural entre los visitantes del parque, especialmente los niños.
+
+<p align="center">
+  <img src="images/about.png" alt="Parque Xanath">
+</p>
+<p align="center">Parque Xanath</p>
+
+## Objetivos
+- Educar a los visitantes sobre la importancia de conservar y proteger el medio ambiente.
+- Desarrollar habilidades prácticas y de liderazgo en los alumnos del CECyTEV a través de su participación en el programa.
+- Establecer una conexión entre la escuela, el parque y la comunidad local.
+
+## Actividades
+Como parte del PAEC, los alumnos del CECyTEV realizan diversas actividades en el Parque Xanath:
+
+1. **Sembrado y cuidado de plantas**
+2. **Actividades lúdicas y recreativas**
+3. **Limpieza y mantenimiento del parque**
+
+## Beneficios
+El PAEC ha generado diversos beneficios tanto para los alumnos del CECyTEV como para la comunidad del Parque Xanath:
+
+- Desarrollo de habilidades de liderazgo, trabajo en equipo y responsabilidad social en los estudiantes.
+- Aprendizaje práctico y divertido sobre cuidado del medio ambiente para los visitantes.
+- Mejor estado y mayor educación ambiental en el Parque Xanath.
+
+## Cómo contribuir
+Si deseas contribuir al proyecto PAEC, puedes:
+
+1. **Descargar o Forkear el repositorio**: Puedes clonar el repositorio en tu máquina local o hacer un fork del mismo en tu cuenta de GitHub.
+2. **Hacer Pull Request**: Si encuentras alguna mejora o corrección que quieras realizar, puedes enviar un pull request con tus cambios.
+
+## Demo
+Puedes ver una demostración del proyecto PAEC en el Parque Xanath visitando este [enlace](https://www.ejemplo.com/paec-demo).
+
+<p align="center">
+  <img src="images/banner.png" alt="Parque Xanath">
+</p>
+<p align="center">Proyecto PAEC</p>
+
+## Copyright
+Este proyecto ha sido desarrollado por Angel Salazar alumno de 4C del Colegio de Estudios Científicos y Tecnológicos del Estado de Veracruz, 02 Papantla CECyTEV. © 2024 CECyTEV. Todos los derechos reservados.
---
+++ js/index.js
@@ -0,0 +1,278 @@
+$(window).on('load', function () {
+  gsap.to('#loader', 1, { y: "-100%" });
+  gsap.to('#loader', 1, { opacity: 0 });
+  gsap.to('#loader', 0, { display: "none", delay: 1 });
+  gsap.to('#header', 0, { display: "block", delay: 1 })
+  gsap.to('#navigation-content', 0, { display: "none" });
+  gsap.to('#navigation-content', 0, { display: "flex", delay: 1 });
+});
+
+$(function () {
+  $('.color-panel').on("click", function (e) {
+    e.preventDefault();
+    $('.color-changer').toggleClass('color-changer-active');
+  });
+
+  $(document).on('click', function (e) {
+    if (!$(e.target).closest('.color-changer').length) {
+      $('.color-changer').removeClass('color-changer-active');
+    }
+  });
+  $('.colors a').on("click", function (e) {
+    e.preventDefault();
+    var attr = $(this).attr("title");
+    console.log(attr);
+    $('head').append('<link rel="stylesheet" href="css/' + attr + '.css">');
+  });
+});
+
+$(function () {
+  $('.menubar').on('click', function () {
+    gsap.to('#navigation-content', .6, { y: 0 });
+  })
+  $('.navigation-close').on('click', function () {
+    gsap.to('#navigation-content', .6, { y: "-100%" });
+  });
+});
+
+$(function () {
+  var TxtRotate = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDevaring = false;
+  };
+
+  TxtRotate.prototype.tick = function () {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDevaring) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDevaring) { delta /= 2; }
+
+    if (!this.isDevaring && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDevaring = true;
+    } else if (this.isDevaring && this.txt === '') {
+      this.isDevaring = false;
+      this.loopNum++;
+      delta = 100;
+    }
+
+    setTimeout(function () {
+      that.tick();
+    }, delta);
+  };
+
+  window.onload = function () {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i = 0; i < elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-rotate');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0em solid #666 ; }";
+    document.body.appendChild(css);
+
+    var spanElement = elements[0];
+
+    function changeColor() {
+      var currentWord = spanElement.innerText.trim();
+      var colorClass = '';
+
+      if (currentWord === 'Aula') {
+        colorClass = 'color2';
+      } else if (currentWord === 'Escuela') {
+        colorClass = 'color3';
+      } else if (currentWord === 'Comunidad') {
+        colorClass = 'color4';
+      }
+
+      spanElement.classList.remove('color2', 'color3', 'color4');
+      if (colorClass !== '') {
+        spanElement.classList.add(colorClass);
+      }
+    }
+
+    changeColor();
+
+    setInterval(function () {
+      changeColor();
+    }, parseInt(period, 1200) || 1);
+  };
+});
+
+$(function () {
+  $('#about-link').on('click', function () {
+    gsap.to('#navigation-content', 0, { display: "none", delay: .7 });
+    gsap.to('#navigation-content', 0, { y: '-100%', delay: .7 });
+    gsap.to('#header', 0, { display: "none" });
+    gsap.to('#gallery', 0, { display: "none" });
+    gsap.to('#portfolio', 0, { display: "none" });
+    gsap.to('#breaker', 0, { display: "block" });
+    gsap.to('#breaker-two', 0, { display: "block", delay: .1 });
+    gsap.to('#contact', 0, { display: "none" });
+    gsap.to('#breaker', 0, { display: "none", delay: 2 });
+    gsap.to('#breaker-two', 0, { display: "none", delay: 2 });
+    gsap.to('#about', 0, { display: "block", delay: .7 });
+    gsap.to('#navigation-content', 0, { display: 'flex', delay: 2 });
+  });
+
+  $('#contact-link').on('click', function () {
+    gsap.to('#navigation-content', 0, { display: "none", delay: .7 });
+    gsap.to('#navigation-content', 0, { y: '-100%', delay: .7 });
+    gsap.to('#header', 0, { display: "none" });
+    gsap.to('#about', 0, { display: "none" });
+    gsap.to('#gallery', 0, { display: "none" });
+    gsap.to('#portfolio', 0, { display: "none" });
+    gsap.to('#breaker', 0, { display: "block" });
+    gsap.to('#breaker-two', 0, { display: "block", delay: .1 });
+    gsap.to('#breaker', 0, { display: "none", delay: 2 });
+    gsap.to('#breaker-two', 0, { display: "none", delay: 2 });
+    gsap.to('#contact', 0, { display: "block", delay: .7 });
+    gsap.to('#navigation-content', 0, { display: 'flex', delay: 2 });
+  });
+  $('#portfolio-link').on('click', function () {
+    gsap.to('#navigation-content', 0, { display: "none", delay: .7 });
+    gsap.to('#navigation-content', 0, { y: '-100%', delay: .7 });
+    gsap.to('#header', 0, { display: "none" });
+    gsap.to('#about', 0, { display: "none" });
+    gsap.to('#contact', 0, { display: "none" });
+    gsap.to('#gallery', 0, { display: "none" });
+    gsap.to('#breaker', 0, { display: "block" });
+    gsap.to('#breaker-two', 0, { display: "block", delay: .1 });
+    gsap.to('#breaker', 0, { display: "none", delay: 2 });
+    gsap.to('#breaker-two', 0, { display: "none", delay: 2 });
+    gsap.to('#portfolio', 0, { display: "block", delay: .7 });
+    gsap.to('#navigation-content', 0, { display: 'flex', delay: 2 });
+  });
+
+  $('#gallery-link').on('click', function () {
+    gsap.to('#navigation-content', 0, { display: "none", delay: .7 });
+    gsap.to('#navigation-content', 0, { y: '-100%', delay: .7 });
+    gsap.to('#header', 0, { display: "none" });
+    gsap.to('#about', 0, { display: "none" });
+    gsap.to('#portfolio', 0, { display: "none" });
+    gsap.to('#contact', 0, { display: "none" });
+    gsap.to('#breaker', 0, { display: "block" });
+    gsap.to('#breaker-two', 0, { display: "block", delay: .1 });
+    gsap.to('#breaker', 0, { display: "none", delay: 2 });
+    gsap.to('#breaker-two', 0, { display: "none", delay: 2 });
+    gsap.to('#gallery', 0, { display: "block", delay: .7 });
+    gsap.to('#navigation-content', 0, { display: 'flex', delay: 2 });
+  });
+
+  $('#home-link').on('click', function () {
+    gsap.to('#navigation-content', 0, { display: "none", delay: .7 });
+    gsap.to('#navigation-content', 0, { y: '-100%', delay: .7 });
+    gsap.to('#header', 0, { display: "none" });
+    gsap.to('#about', 0, { display: "none" });
+    gsap.to('#portfolio', 0, { display: "none" });
+    gsap.to('#contact', 0, { display: "none" });
+    gsap.to('#gallery', 0, { display: "none" });
+    gsap.to('#breaker', 0, { display: "block" });
+    gsap.to('#breaker-two', 0, { display: "block", delay: .1 });
+    gsap.to('#breaker', 0, { display: "none", delay: 2 });
+    gsap.to('#breaker-two', 0, { display: "none", delay: 2 });
+    gsap.to('#header', 0, { display: "block", delay: .7 });
+    gsap.to('#navigation-content', 0, { display: 'flex', delay: 2 });
+  });
+});
+
+$(function () {
+  var body = document.querySelector('body');
+  var $cursor = $('.cursor')
+  function cursormover(e) {
+
+    gsap.to($cursor, {
+      x: e.clientX,
+      y: e.clientY,
+      stagger: .002
+    })
+  }
+  function cursorhover(e) {
+    gsap.to($cursor, {
+      scale: 1.4,
+      opacity: 1
+    })
+
+  }
+  function cursor(e) {
+    gsap.to($cursor, {
+      scale: 1,
+      opacity: .6
+    })
+  }
+  $(window).on('mousemove', cursormover);
+  $('.menubar').hover(cursorhover, cursor);
+  $('a').hover(cursorhover, cursor);
+  $('.navigation-close').hover(cursorhover, cursor);
+});
+
+$(function () {
+  const fullImgBox = document.getElementById("fullImgBox"),
+    imgElement = document.getElementById("img-element"),
+    rightBtn = document.getElementById("right-btn"),
+    leftBtn = document.getElementById("left-btn"),
+    closeImg = document.getElementById("closeImg");
+
+  var listImg = [...document.querySelectorAll(".image")];
+  var indexImg = 0;
+
+  const sliderBoxes = document.querySelectorAll('.slider-box');
+  const viewImgBtns = document.querySelectorAll('.button button');
+
+  viewImgBtns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+      clicked(index);
+      fullImgBox.style.display = "flex";
+      imgElement.src = sliderBoxes[index].querySelectorAll('.image')[0].src;
+    });
+  });
+
+  function clicked(position) {
+    indexImg = 0;
+    sliderBoxes.forEach((box, i) => {
+      if (i === position) {
+        listImg = [...box.querySelectorAll('.image')];
+      }
+    });
+    imgElement.src = listImg[indexImg].src;
+  }
+
+  rightBtn.addEventListener("click", e => {
+    if (indexImg >= listImg.length - 1) {
+      indexImg = -1;
+    }
+    indexImg++;
+    imgElement.src = listImg[indexImg].src;
+  });
+
+  leftBtn.addEventListener("click", e => {
+    if (indexImg <= 0) {
+      indexImg = listImg.length;
+    }
+    indexImg--;
+    imgElement.src = listImg[indexImg].src;
+  });
+
+  closeImg.addEventListener("click", e => fullImgBox.style.display = "none");
+});
